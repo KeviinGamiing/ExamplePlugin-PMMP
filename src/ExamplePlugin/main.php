583cf7b75f8b0bbe0292088172b12fc7d7a83170
KeviinGamiing <52319074+KeviinGamiing@users.noreply.github.com>
2019-06-28 19:51:55 +0800

    Main.php

---
+++ src/ExamplePlugin/main.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ExamplePlugin;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class main extends PluginBase {
+    
+    public function onLoad(): void{
+        $this->getLogger()->info(C::GOLD . "Loading Calypso Core..." . "Plugin was made by KeviinGamiing");
+    }
+    public function onEnable(): void{
+        $this->getLogger()->info(C::GREEN . "Plugin has been Enabled!");
+    }
+    public function onDisable(): void{
+        $this->getLogger()->info(C::RED . "Plugin has been Disabled");
+    }
+    public function onCommand(CommandSender $sender, Command $cmd, String $Label, array $args): bool{
+        if($cmd->getName()){
+            switch($args(count) === 0){
+              case "example":
+                $player->sendMessage(C::GREEN . "This is an Example");
+                $player->addTitle(C::GREEN . "Example" . "Plugin");
+                break;
+            }
+        return true;
+        }
+    }
+}
